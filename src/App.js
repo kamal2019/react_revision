faf24346131a87c77f2f1ea161732c7a67ca01e3
kamal aryal <mraryalkamal2011@gmail.com>
2021-12-03 22:19:39 +0545

    states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="kamal" heroName="batman"/>
-      <Welcome name="laxmi"/>
+      <Message/>
+      {/* /* <Greet name="kamal" heroName="batman"/>
+      <Welcome name="laxmi"/> */}
     </div>
   );
 }
