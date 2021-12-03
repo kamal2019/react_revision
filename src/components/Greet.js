b3973bf66e6696af54b164c5ed47007bd1790631
kamal aryal <mraryalkamal2011@gmail.com>
2021-12-03 17:25:56 +0545

    functional components

---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello kamal</h1>
+// }
+// export default Greet
+
+const Greet = () => <h1>Hello kamal aryal</h1>
+export default Greet
