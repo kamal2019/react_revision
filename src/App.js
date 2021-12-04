c273507843894a5acf22c4ba069ff6dde5b989ef
kamal aryal <mraryalkamal2011@gmail.com>
2021-12-04 07:45:38 +0545

    css styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Stylesheet from './components/Stylesheet';
 
 function App() {
   return (
     <div className="App">
-      <Message/>
+      <Stylesheet primary={true}/>
+      {/* <Message/> */}
       {/* /* <Greet name="kamal" heroName="batman"/>
       <Welcome name="laxmi"/> */}
     </div>
