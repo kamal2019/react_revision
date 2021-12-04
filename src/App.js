bca8e164479f659545fe18ffb82d5d7851fd89d1
kamal aryal <mraryalkamal2011@gmail.com>
2021-12-04 13:02:04 +0545

    context

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Stylesheet from './components/Stylesheet';
 import Forms from './components/Forms';
+import ComponentC from './components/ComponentC';
+
 
 function App() {
   return (
     <div className="App">
-      <Forms/>
+      <ComponentC/>
+      {/* <Forms/> */}
       {/* <Stylesheet primary={true}/> */}
       {/* <Message/> */}
       {/* /* <Greet name="kamal" heroName="batman"/>
